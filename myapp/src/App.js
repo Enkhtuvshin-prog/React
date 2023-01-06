4cf46dff5ec9670194b7924e2facbb58a835ca25
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2023-01-06 12:30:53 +0800

    home...1.6

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import Home from "./components/page/home/home";
 import Detail from "./components/page/detail/detail";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Button } from 'bootstrap';
-// import UserCard from './components/UserCard';
 
 function App() {
   return (
